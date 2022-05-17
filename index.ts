925186ebca350e71396509a71ccb99d0ecf434b7
yuttasin klungnak <yuttasin.klu@biggestfan.net>
2022-05-17 16:23:05 +0700

    subscription on testnet worked

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
+import Web3 from "web3";
+
 console.log("start subscriptions");
+// const web3 = new Web3("wss://smartbch.regtest.actorforth.org/ws/");
+const web3 = new Web3("wss://matic-testnet-archive-ws.bwarelabs.com");
+const subscription = web3.eth.subscribe(
+  "pendingTransactions",
+  function (error, result) {
+    if (!error) {
+      console.log(result);
+
+      return;
+    }
+
+    console.error(error);
+  }
+);
